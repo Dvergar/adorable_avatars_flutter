f925e8c85dfd896bb18e9fddd07621ab35945dfa
Dvergar <laurent.rd@gmail.com>
2020-01-23 13:52:20 +0100

    basic image loader

---
+++ lib/avatar_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+class AvatarBloc {
+  int size = 285;
+  String identifier = "abott@adorable.io";
+
+  final urlController =
+      StreamController.broadcast(); // create a StreamController
+
+  Stream get stream => urlController.stream; // create a getter for our stream
+
+
+
+  void updateUrl() {
+    urlController.sink
+        .add('https://api.adorable.io/avatars/$size/$identifier.png');
+  }
+
+  void updateSize(int newSize) {
+    this.size = newSize;
+    updateUrl();
+  }
+
+  void updateIdentifier(String newIdentifier) {
+    this.identifier = newIdentifier;
+    updateUrl();
+  }
+
+  void dispose() {
+    urlController.close(); // close our StreamController
+  }
+}
+
+final avatarBloc = AvatarBloc();
